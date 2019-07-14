318ee313bb35f58755310dc04bb4a946f468d9fb
chak-kit <kitchakk93@gmail.com>
2019-07-14 21:58:18 +0300

    HW9 tasks 1-4

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,3 +1,4 @@
+//0
 function getNumbers(value) {
     const returns = [];
     for (let key of value) {
@@ -8,6 +9,7 @@ function getNumbers(value) {
     return returns;
 }
 
+//1
 getNumbers('n1um3ber95');
 
 function findTypes() {
@@ -25,11 +27,46 @@ function findTypes() {
 
 findTypes('number', 7, null);
 
-function executeforEach() {
+//2
+function executeforEach(arr, func) {
+    let result = [];
 
-        for (let element of arguments[0]){
-            arguments[1](element);
-        }
+    for (let element of arr) {
+        result.push(func(element));
+    }
+
+    return result;
 }
 
-executeforEach([1, 2, 3], function (el) {console.log(el)});
+executeforEach([1, 2, 3], function (el) {
+    console.log(el)
+});
+
+
+//3
+function mapArray(arr, func) {
+    return executeforEach(arr, func);
+}
+
+let res = mapArray([2, 5, 8], function (el) {
+    return el + 3
+});
+console.log(res);
+
+//4
+function filterArray(arr, func) {
+    let filtered = executeforEach(arr, func);
+    let result = [];
+    for (let i = 0; i < filtered.length; i++) {
+        if (filtered[i]) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+
+}
+
+console.log(filterArray([2, 5, 8], function (el) {
+    return el > 3
+}));
